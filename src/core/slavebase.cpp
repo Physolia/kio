ec7f33f54141ae6335c6b161001297016e9f004e
David Faure <faure@kde.org>
2022-01-30 19:34:34 +0100

    Implement running KIO workers in-process using a thread
    This commit enables it for kio_file, for testing, but
    * I'm seeing one last problem in jobtest
    * We might want to only do this on Android (see discussion in task)
    
    Task: https://phabricator.kde.org/T12214

--- src/core/slavebase.cpp
+++ src/core/slavebase.cpp
@@ -109,9 +109,10 @@ public:
     bool resume : 1;
     bool needSendCanResume : 1;
     bool onHold : 1;
-    bool wasKilled : 1;
     bool inOpenLoop : 1;
-    bool exit_loop : 1;
+    std::atomic<bool> wasKilled;
+    std::atomic<bool> exit_loop;
+    std::atomic<bool> runInThread;
     MetaData configData;
     KConfig *config = nullptr;
     KConfigGroup *configGroup = nullptr;
@@ -282,14 +283,12 @@ SlaveBase::SlaveBase(const QByteArray &protocol, const QByteArray &pool_socket, 
     d->needSendCanResume = false;
     d->mapConfig = QMap<QString, QVariant>();
     d->onHold = false;
-    d->wasKilled = false;
     //    d->processed_size = 0;
     d->totalSize = 0;
     connectSlave(QFile::decodeName(app_socket));
 
     d->remotefile = nullptr;
     d->inOpenLoop = false;
-    d->exit_loop = false;
 }
 
 SlaveBase::~SlaveBase()
@@ -723,14 +722,18 @@ void SlaveBase::mimeType(const QString &_type)
     mOutgoingMetaData.clear();
 }
 
-void SlaveBase::exit()
+void SlaveBase::exit() // possibly called from another thread, only use atomics in here
 {
     d->exit_loop = true;
-    // Using ::exit() here is too much (crashes in qdbus's qglobalstatic object),
-    // so let's cleanly exit dispatchLoop() instead.
-    // Update: we do need to call exit(), otherwise a long download (get()) would
-    // keep going until it ends, even though the application exited.
-    ::exit(255);
+    if (d->runInThread) {
+        d->wasKilled = true;
+    } else {
+        // Using ::exit() here is too much (crashes in qdbus's qglobalstatic object),
+        // so let's cleanly exit dispatchLoop() instead.
+        // Update: we do need to call exit(), otherwise a long download (get()) would
+        // keep going until it ends, even though the application exited.
+        ::exit(255);
+    }
 }
 
 void SlaveBase::warning(const QString &_msg)
@@ -1552,6 +1555,11 @@ void SlaveBase::virtual_hook(int id, void *data)
         break;
     }
     }
+}
+
+void SlaveBase::setRunInThread(bool b)
+{
+    d->runInThread = b;
 }
 
 void SlaveBase::lookupHost(const QString &host)
